f1b58841e3cb650e941cdf40371d42f67b1fffba
Zan <zanmadtest@gmail.com>
2021-01-30 19:46:15 +0200

    Add project files.

---
+++ NeuralNetworks101/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NeuralNetworks101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
